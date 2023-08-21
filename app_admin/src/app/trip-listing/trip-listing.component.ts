7c9885f0bb3388ee2b85dd9d2c19ec595e42e414
Joshua Asari <josh.asari@gmail.com>
2023-08-20 22:01:09 -0400

    module7

--- app_admin/src/app/trip-listing/trip-listing.component.ts
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 import { TripDataService } from '../services/trip-data.service';
-import { trips } from '../data/trips';
+import { Trip } from '../models/trip';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-trip-listing',
@@ -11,12 +12,13 @@ import { trips } from '../data/trips';
 })
 export class TripListingComponent implements OnInit {
 
-  trips: Array<any> = trips;
+  trips: Trip[];
 
   message: string;
 
   constructor(
     private tripDataService: TripDataService,
+    private authService: AuthenticationService,
     private router: Router
     ) { }
 
@@ -34,6 +36,10 @@ export class TripListingComponent implements OnInit {
         this.message = foundTrips.length > 0 ? '' : 'No trips found';
         this.trips = foundTrips;
       });
+  }
+
+  public isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
   }
 
   ngOnInit() {
